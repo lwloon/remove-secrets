97f18c2f0cbcd9116964741fb7b8ece81ec6a808
infeniontech.dylanlai <dylanlai@infeniontech.com>
2018-10-03 17:05:12 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://lwloon:weiloon123@ds121753.mlab.com:21753/remove-secrete
+# export MONGODB_CREDENTIALS=mongodb://lwloon:weiloon123@ds121753.mlab.com:21753/remove-secrete
 
 # finally, run the app in dev mode...
 npm run dev
